8cc1f34e6ffbc0105f38e10976f85c546ee6d215
shubh3365 <shubhamkharade44@gmail.com>
2026-08-06 17:06:46 +0530

    Added Exam Instruction Model & DTOs

---
+++ BTDS/DTOs/ExamInstructionCreateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BTDS.DTOs
+{
+    public class ExamInstructionCreateDto
+    {
+        public long ExamId { get; set; }
+
+        public string Instruction { get; set; } = string.Empty;
+
+        public int SequenceNo { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ BTDS/DTOs/ExamInstructionResponseDto.cs
@@ -0,0 +1,29 @@
+﻿namespace BTDS.DTOs
+{
+    public class ExamInstructionResponseDto
+    {
+        public long InstructionId { get; set; }
+
+        public long ExamId { get; set; }
+
+        public string Instruction { get; set; } = string.Empty;
+
+        public int SequenceNo { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int UpdatedBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public int DeletedBy { get; set; }
+
+        public DateTime DeletedAt { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
